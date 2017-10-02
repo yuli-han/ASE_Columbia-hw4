a6dbfe3fe3cef173e6d96f7b57a03f37d53a39f1
Sophiazys <yz3083@columbia.edu>
2017-10-01 20:45:21 -0400

    add aws mysql db

--- server/server.go
+++ server/server.go
@@ -11,17 +11,26 @@ package main
 
 import (
 	"flag"
+    "fmt"
 	"html/template"
 	"log"
 	"net/http"
-    "strconv"
+    "strings"
 	"github.com/gorilla/websocket"
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options
+var db *gorm.DB
 
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -29,26 +38,26 @@ func echo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer c.Close()
-    var user userbasicinfo 
-	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
-		}
-		log.Printf("recv: %s", message)
-        /////////handle request here//////////
-        reply := handlemsg(message,user)      
-        //////////////////////////////////////////
-        err = c.WriteMessage(mt, []byte(reply))
-		
+	
+    for {        		
+        mt, message, err := c.ReadMessage()  
+        fmt.Println("mt",mt)     
+        err = c.WriteMessage(mt, []byte("please input name"))
+		check(err)
+        name := string(message[:])
 
+        if !strings.Contains(name, "2017-"){
+            user := User{Name: name, Age: 22}
+            db.Create(&user)
+        }
+        var users []User
+        db.Find(&users) 
 
-        //////////////////////////////////////////
-        if err != nil {
-			log.Println("write:", err)
-			break
-		}
+        for i,_:=range users{
+        list:= forminfo(users[i])
+        err = c.WriteMessage(mt, []byte(list ))        
+        }	                        
+        
 	}
 }
 
@@ -57,11 +66,17 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	flag.Parse()
+    fmt.Println("start")
+    db, _= gorm.Open("mysql", "yz3083:Healthpet@(healthpet.cf82kfticiw1.us-east-1.rds.amazonaws.com:3306)/Healthpet?charset=utf8&parseTime=True&loc=Local")
+    defer db.Close()
+    db.AutoMigrate(&User{})
+    	
+    flag.Parse()
 	log.SetFlags(0)
 	http.HandleFunc("/echo", echo)
 	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(*addr, nil))
+
 }
 
 var homeTemplate = template.Must(template.New("").Parse(`
@@ -139,35 +154,16 @@ You can change the message and send multiple times.
 `))
 
 
-
-func handlemsg( request []byte, user userbasicinfo ) string{
-   var reply string
-   switch userrequest := string(request[:]);userrequest{
-    case "Log in":
-        reply = "what is your name"
-    case "Name":
-        reply ="what is your age"
-        user.Name = userrequest 
-    case "Age":
-        reply ="what is your Gender"
-        age, _:= strconv.Atoi(userrequest)
-        user.Age = age
-    default:
-        // freebsd, openbsd,
-        // plan9, windows...
-        reply ="Welcome to Health pet"
-   }
-   
-   return reply
-   
+func forminfo(input User) string {
+    reply:= input.Name +"\n"
+    return reply
 }
 
-type userbasicinfo struct {
+
+type User struct {
+    gorm.Model
     Name string
     Age  int
-    Gender string
-    //Weight float
-    //Height float
-    //Username string
-    //password string    
+    
+    
 }
